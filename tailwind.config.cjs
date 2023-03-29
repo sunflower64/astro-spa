51e3fa7db637ba86205eddf34569de2e38e4f5ed
sunflower64 <71190065+sunflower64@users.noreply.github.com>
2023-03-28 21:46:53 -0300

    ceo adjustments

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
     colors: {
-      'white': '#e4e4e7',
+      'white': '#f4f4f5',
       'gray': '#d4d4d8',
       'primary':'#1d4ed8',
       'primary-hover': '#1e3a8a',
