54f1d179b64efd29499916a593a191bb72a212c0
sunflower64 <71190065+sunflower64@users.noreply.github.com>
2023-03-28 22:09:36 -0300

    Navbar mobile adjusment

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -6,7 +6,7 @@ export default function DropdownMenu() {
   return (
     <Menu as="div" className="relative lg:hidden inline-block text-left my-auto">
       <div className="flex">
-        <Menu.Button className="inline-flex justify-center rounded-md border border-zinc-400 my-auto" aria-label="menu">
+        <Menu.Button className="inline-flex justify-center rounded-md border border-white my-auto" aria-label="menu">
           <IoMenu className="h-8 w-8"/>
         </Menu.Button>
       </div>
@@ -20,12 +20,12 @@ export default function DropdownMenu() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md border border-zinc-400 bg-zinc-700">
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md border border-white bg-primary">
             <div className="grid grid-cols-1 divide-y px-3 py-2 uppercase font-bold text-zinc-100">
-              <a className="" href="#Inicio">Inicio</a>
-              <a className="" href="#Servicios">Servicios</a>
-              <a className="" href="#Sobre">Sobre Nosotros</a>
-              <a className="" href="#Contacto">Contacto</a>
+              <a className="" href="/">Home</a>
+              <a className="" href="#Services">Services</a>
+              <a className="" href="#About">About Us</a>
+              <a className="" href="#Contact">Contact</a>
           </div>
         </Menu.Items>
       </Transition>
